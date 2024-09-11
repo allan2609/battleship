99ba0ff641f56780350de2e86cb96f0ea08320c4
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-09-11 17:31:00 +0300

    Add tests for isSunk() method

--- src/Ship.test.js
+++ src/Ship.test.js
@@ -13,3 +13,24 @@ test("calling hit() doesn't increment hitsTaken if the ship is already sunk", ()
   ship.hit();
   expect(ship.hitsTaken).toEqual(1);
 });
+
+test("isSunk() returns false when the ship hasn't taken enough hits", () => {
+  const ship = new Ship(5);
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(false);
+});
+
+test("isSunk() returns true when the ship has taken hits equal to its length", () => {
+  const ship = new Ship(2);
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("isSunk() updates the sunk property when the ship is sunk", () => {
+  const ship = new Ship(2);
+  ship.hit();
+  ship.hit();
+  expect(ship.sunk).toBe(true);
+});
