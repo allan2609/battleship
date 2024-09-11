d3181db0ba05239fb2e78ca6179907af3e1e007d
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-09-11 17:26:22 +0300

    Add second test for Ship

--- src/Ship.test.js
+++ src/Ship.test.js
@@ -6,3 +6,10 @@ test("calling hit() increments hitsTaken property", () => {
   ship.hit();
   expect(ship.hitsTaken).toEqual(2);
 });
+
+test("calling hit() doesn't increment hitsTaken if the ship is already sunk", () => {
+  const ship = new Ship(1);
+  ship.hit();
+  ship.hit();
+  expect(ship.hitsTaken).toEqual(1);
+});
