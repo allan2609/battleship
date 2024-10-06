c571733cc28e64a3ddfe08478497973ba93ab8f8
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-10-06 18:22:52 +0300

    Write tests for receiveAttack method

--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -8,3 +8,32 @@ test("placeShip places a ship on the gameboard at specified coordinates", () => 
   
   expect(gameboard.getShipAt(2, 2)).toBe(ship);
 });
+
+test("receiveAttack sends the hit function to the correct ship", () => {
+  const gameboard = new Gameboard(10);
+  const ship = new Ship(3);
+  gameboard.placeShip(ship, 2, 2);
+  gameboard.receiveAttack(2, 2);
+  expect(ship.hitsTaken).toEqual(1);
+});
+
+test("receiveAttack records the coordinates of a missed shot", () => {
+  const gameboard = new Gameboard(10);
+  gameboard.receiveAttack(5, 5);
+  expect(gameboard.missedShots).toContainEqual([5, 5]);
+});
+
+test("receiveAttack does not allow duplicate attacks on the same coordinate", () => {
+  const gameboard = new Gameboard(10);
+  const ship = new Ship(3);
+  gameboard.placeShip(ship, 2, 2);
+  gameboard.receiveAttack(2, 2);
+  gameboard.receiveAttack(2, 2);
+  expect(ship.hitsTaken).toEqual(1);
+  expect(gameboard.missedShots.length).toEqual(0);
+});
+
+test("receiveAttack throws an error if the coordinates are out of bounds", () => {
+  const gameboard = new Gameboard(10);
+  expect(() => gameboard.receiveAttack(11, 5)).toThrow("Coordinates are out of bounds");
+});
