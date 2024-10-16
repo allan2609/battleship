a1e050f77d93bb7e48e58d0b9936f2c2cb265946
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-10-16 16:51:23 +0300

    Add support for vertical ship placement

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -6,24 +6,39 @@ class Gameboard {
     this.attackedPositions = [];
   }
 
-  placeShip(ship, row, column) {
-    // Assuming ships are placed horizontally for simplicity.
-    if (column + ship.length > this.size) {
-      throw new Error("Ship cannot be placed out of bounds");
-    }
-  
-    for (let i = 0; i < ship.length; i++) {
-      if (this.board[row][column + i] !== null) {
-        throw new Error("Cannot place ship, position already occupied");
+  placeShip(ship, row, column, isVertical = false) {
+    if (isVertical) {
+      if (row + ship.length > this.size) {
+        throw new Error("Ship cannot be placed out of bounds");
+      }
+    } else {
+      if (column + ship.length > this.size) {
+        throw new Error("Ship cannot be placed out of bounds");
       }
     }
   
     for (let i = 0; i < ship.length; i++) {
-      this.board[row][column + i] = ship;
+      if (isVertical) {
+        if (this.board[row + i][column] !== null) {
+          throw new Error("Cannot place ship, position already occupied");
+        }
+      } else {
+        if (this.board[row][column + i] !== null) {
+          throw new Error("Cannot place ship, position already occupied");
+        }
+      }
+    }
+  
+    for (let i = 0; i < ship.length; i++) {
+      if (isVertical) {
+        this.board[row + i][column] = ship;
+      } else {
+        this.board[row][column + i] = ship;
+      }
     }
   
     this.ships.push(ship);
-  }
+  }  
 
   getShipAt(row, column) {
     return this.board[row][column];
@@ -33,7 +48,7 @@ class Gameboard {
     if (row < 0 || row >= this.size || column < 0 || column >= this.size) {
       throw new Error("Coordinates are out of bounds");
     }
-    
+
     if (this.attackedPositions.some(pos => pos.row === row && pos.column === column)) {
       throw new Error("Position has already been attacked");
     }
