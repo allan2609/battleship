486e9438efa6e787a7598a736795eb03a9225de2
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-10-22 16:20:26 +0300

    Create tests for player type

--- src/Player.test.js
+++ src/Player.test.js
@@ -1,3 +1,12 @@
-import Ship from "./Ship";
-import Gameboard from "./Gameboard";
 import Player from "./Player";
+import Gameboard from "./Gameboard";
+
+test("creates a human player", () => {
+  const player = new Player("human");
+  expect(player.type).toBe("human");
+});
+
+test("creates a computer player", () => {
+  const player = new Player("computer");
+  expect(player.type).toBe("computer");
+});
