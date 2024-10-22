9eccece2dd08c0caf23b5701f457e2ad3c84de25
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-10-22 16:31:13 +0300

    Test if a player has its own gameboard

--- src/Player.test.js
+++ src/Player.test.js
@@ -10,3 +10,13 @@ test("creates a computer player", () => {
   const player = new Player("computer");
   expect(player.type).toBe("computer");
 });
+
+test("player has a gameboard", () => {
+  const player = new Player("human");
+  expect(player.gameboard).toBeDefined();
+});
+
+test("player's gameboard is of correct size", () => {
+  const player = new Player("human");
+  expect(player.gameboard.size).toBe(10);
+});
