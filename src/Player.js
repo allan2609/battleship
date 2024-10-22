1eddfd3cfb77e5d63de48a04482a6fa4308cbfaf
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-10-22 16:20:50 +0300

    Create Player class

--- src/Player.js
+++ src/Player.js
@@ -1,2 +1,10 @@
-import Ship from "./Ship";
 import Gameboard from "./Gameboard";
+
+class Player {
+  constructor(type) {
+    this.type = type;
+    this.gameboard = new Gameboard(10);
+  }
+}
+
+export default Player;
