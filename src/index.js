f9e45d6e94565e21b0b47238da15b2d0a306a39b
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-10-27 17:50:10 +0200

    Create players and populate both gameboards with predetermined coordinates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+import Player from "./Player";
+import Ship from "./Ship";
+
+const player = new Player("human");
+const computer = new Player("computer");
+
+const playerShip1 = new Ship(3);
+const playerShip2 = new Ship(4);
+const computerShip1 = new Ship(3);
+const computerShip2 = new Ship(4);
+
+player.gameboard.placeShip(playerShip1, 2, 2, false);
+player.gameboard.placeShip(playerShip2, 5, 5, true);
+computer.gameboard.placeShip(computerShip1, 1, 1, false);
+computer.gameboard.placeShip(computerShip2, 6, 3, true);
