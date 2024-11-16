dc155c8dc8e731e9832819738cead10b9fcde04a
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-11-16 15:10:52 +0200

    Delegate attack handling to GameController and set up initial rendering

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { renderPlayerBoard, renderComputerBoard } from "./GameboardRenderer";
 const player = new Player("human");
 const computer = new Player("computer");
 
-const gameController = new GameController(player, computer);
-
 const playerShip1 = new Ship(3);
 const playerShip2 = new Ship(4);
 const computerShip1 = new Ship(3);
@@ -19,17 +17,18 @@ player.gameboard.placeShip(playerShip2, 5, 5, true);
 computer.gameboard.placeShip(computerShip1, 1, 1, false);
 computer.gameboard.placeShip(computerShip2, 6, 3, true);
 
+const gameController = new GameController(player, computer);
+
+renderPlayerBoard();
+renderComputerBoard();
+
 document.querySelector(".computer-board").addEventListener("click", (e) => {
   const row = parseInt(e.target.dataset.row, 10);
   const column = parseInt(e.target.dataset.column, 10);
 
   if (!isNaN(row) && !isNaN(column)) {
-    computer.gameboard.receiveAttack(row, column);
-    renderComputerBoard();
+    gameController.handleAttack(row, column);
   }
 });
-
-renderPlayerBoard();
-renderComputerBoard();
 
 export { player, computer };
